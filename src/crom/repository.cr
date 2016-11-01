d63ebe9acba1079155af5214c1913fc1f876e7ac
Arnaud Fernandés <arnaud.fernandes@laposte.net>
2016-11-01 17:41:27 +0100

    Add namespace

--- src/crom/repository.cr
+++ src/crom/repository.cr
@@ -2,12 +2,15 @@ module CROM
   abstract class Repository(T)
     @model : Model.class
 
-    def initialize(@container : Container)
+    getter :container
+
+    def initialize(@container : CROM::Container)
       @model = T
     end
 
     abstract def create(**attributes) : T
     abstract def update(model : T)
     abstract def delete(model : T)
+    abstract def persist
   end
 end
